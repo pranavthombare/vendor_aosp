195fe5035efacccddacf70afdd316cf19966a662
ezio84 <brabus84@gmail.com>
2017-12-11 09:49:10 -0500

    Add all Pixel sounds
    pulled from latest O sailfish img
    
    Change-Id: I402094eb0cca23ceb0d6d55328e9e192a414c46a

--- config/common.mk
+++ config/common.mk
@@ -194,5 +194,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=$(LEGENDROM_MOD_VERSION) \
     ro.legendrom.buildtype=$(LEGENROM_BUILD_TYPE)
 
-EXTENDED_POST_PROCESS_PROPS := vendor/aosp/tools/aosp_process_props.py
+# Google sounds
+include vendor/androidx/google/GoogleAudio.mk
 
+EXTENDED_POST_PROCESS_PROPS := vendor/gzosp/tools/gzosp_process_props.py
