a0be28001a47469e58ca8d2b2110588129356cd5
maxwen <max.weninger@gmail.com>
2018-01-02 22:54:58 +0100

    [2/2] omni: add OTA script do delete package cache
    after flashing new image delete contents of /data/system/package_cache/
    to make sure they are recreated. Outdated cache files are the
    reason for the famous 'resource derps'
    
    Change-Id: Iaf1f113a64242254b8fb33bc5b9fdef0c9fdb120

--- config/common.mk
+++ config/common.mk
@@ -22,7 +22,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/aosp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/aosp/prebuilt/common/bin/50-aosp.sh:system/addon.d/50-aosp.sh
+    vendor/aosp/prebuilt/common/bin/50-aosp.sh:system/addon.d/50-aosp.sh \
+    vendor/aosp/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
 
 # Backup services whitelist
 PRODUCT_COPY_FILES += \
